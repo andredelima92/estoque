8c07c003d6d76df30e7f87edd4103dc185a65429
Andre <andredelima92@gmail.com>
2017-01-26 16:35:59 -0200

    novo commit teste

--- app/Controller/UsuariosController.php
+++ app/Controller/UsuariosController.php
@@ -15,7 +15,11 @@ class UsuariosController extends AppController{
     echo $this->template->render(array('this' => new View($this), 'parametros' => $this->parametros));
     exit;
   }
+<<<<<<< Updated upstream
   //Nova alteração
+=======
+  // teste de comentario para o git
+>>>>>>> Stashed changes
   public function alterarSenha(){
     $this->isLogin();
     $this->init();
